516026420aa0332b835aceb2612c9375685fdb15
Kirtiman29 <kirtimangupta29@gmail.com>
2025-07-16 20:44:54 +0530

    frontend done

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:perplexity_clone/themes/colors.dart';
 import 'package:perplexity_clone/widgets/side_nav.dart';
+
+import '../widgets/search_section.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -8,16 +11,85 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(
-        children: [
-          SideBar(),
-          Column(
-            children: [
+        body: Row(
+          children: [
+            SideBar(),
+            Expanded(
+              child: Column(
+                children: [
+                  Expanded(
+                    child: SearchSection()
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 16),
+                    child: Wrap(
+                      alignment: WrapAlignment.center,
+                      spacing: 12,
+                      children: [
+                        Padding(padding: EdgeInsets.symmetric(horizontal: 12),
+                        child: Text(
+                          "Pro",
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: AppColors.textGrey
+                          ),
+                        ),
+                        ),
+                        Padding(padding: EdgeInsets.symmetric(horizontal: 12),
+                          child: Text(
+                            "Enterprise",
+                            style: TextStyle(
+                                fontSize: 14,
+                                color: AppColors.textGrey
+                            ),
+                          ),
+                        ),
+                        Padding(padding: EdgeInsets.symmetric(horizontal: 12),
+                          child: Text(
+                            "Store",
+                            style: TextStyle(
+                                fontSize: 14,
+                                color: AppColors.textGrey
+                            ),
+                          ),
+                        ),
+                        Padding(padding: EdgeInsets.symmetric(horizontal: 12),
+                          child: Text(
+                            "Blog",
+                            style: TextStyle(
+                                fontSize: 14,
+                                color: AppColors.textGrey
+                            ),
+                          ),
+                        ),
+                        Padding(padding: EdgeInsets.symmetric(horizontal: 12),
+                          child: Text(
+                            "Careers",
+                            style: TextStyle(
+                                fontSize: 14,
+                                color: AppColors.textGrey
+                            ),
+                          ),
+                        ),
+                        Padding(padding: EdgeInsets.symmetric(horizontal: 12),
+                          child: Text(
+                            "English (English)",
+                            style: TextStyle(
+                                fontSize: 14,
+                                color: AppColors.textGrey
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                )
 
-            ],
-          )
-        ],
-      )
+
+                ],
+              ),
+            )
+          ],
+        )
     );
   }
 }
