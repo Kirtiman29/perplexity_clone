5f7ff98643cf780cd5ffe1fc8d740f20a6f1a80e
Kirtiman29 <kirtimangupta29@gmail.com>
2025-07-17 19:40:50 +0530

    frontend done

--- server/main.py
+++ server/main.py
@@ -2,15 +2,19 @@ from fastapi import FastAPI
 from pydantic_models.chat_body import  ChatBody
 
 from services.search_services import SearchServices
-
+from services.sort_source_service import SortSourceService 
 
 app = FastAPI() 
 
 search_srvices = SearchServices()
+sort_source_service = SortSourceService()
 
 @app.post("/chat")
 def chat_endpoint(body: ChatBody):
     
-    search_srvices.web_search(body.query)
+    search_results=search_srvices.web_search(body.query)
+    #print(search_results)
 
+    sorted_results=sort_source_service.sort_service(body.query, search_results)
+    print(sorted_results)
     return body.query
--- server/services/search_services.py
+++ server/services/search_services.py
@@ -1,7 +1,25 @@
 
 from config import Settings
+from tavily import TavilyClient
+import trafilatura
 settings = Settings()
+tavily_client = TavilyClient(api_key=settings.TAVILY_API_KEY)
 
 class SearchServices:
     def web_search(self,query: str):
-        print(Settings().TAVILY_API_KEY)
+
+        results =[]
+        response = tavily_client.search(query,max_results=10)
+
+        search_result=response.get("results",[])
+        for result in search_result:
+           downloaded= trafilatura.fetch_url(result.get('url'))
+
+           content = trafilatura.extract(downloaded,include_comments=False)
+
+           results.append({
+               "title" : result.get("title", ""),
+               "url": result.get("url"),
+               "content" : content
+           })
+        return results
